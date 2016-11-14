96e7b22ed8c97e22b6b084482e1fab6e022b0e41
lindsaymkelly <lindzlady@gmail.com>
2016-11-13 20:49:47 -0500

    Add edit functionality for articles

--- lmkrailsblog/app/controllers/articles_controller.rb
+++ lmkrailsblog/app/controllers/articles_controller.rb
@@ -12,8 +12,12 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
-    @article = Article.new(params.require(:article).permit(:title, :text))
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article 
@@ -22,5 +26,19 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else 
+      render 'edit'
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 
 end
